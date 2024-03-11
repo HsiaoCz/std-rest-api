fe1d4fd38aa08752793c866b3132742d380df9fa
HsiaoCz <984274788@qq.com>
2024-03-11 20:48:41 +0800

    kafka

--- gokafka/cmd/main.go
+++ gokafka/cmd/main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
 
 func main() {
-	fmt.Println("all is well")
+	// kafka connect
+	topic := "HVSE"
+	go ProduceKafka(topic)
+	go ConsumeKafka(topic)
+	time.Sleep(time.Second * 3)
+}
+
+func ProduceKafka(topic string) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		log.Printf("failed to create producer: %s\n", err)
+		return
+	}
+	defer p.Close()
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivery message to %v\n", ev.TopicPartition)
+				}
+
+			}
+		}
+	}()
+	p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          []byte("ffoo"),
+	}, nil)
+	p.Flush(15 * 1000)
+}
+
+func ConsumeKafka(topic string) {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		fmt.Printf("kafka new consumer err:%v\n", err)
+		return
+	}
+
+	c.SubscribeTopics([]string{"myTopic", "^aRegex.*[Tt]opic"}, nil)
+
+	// A signal handler or similar could be used to set this to false to break the loop.
+	run := true
+	for run {
+		msg, err := c.ReadMessage(time.Second)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else if !err.(kafka.Error).IsTimeout() {
+			// The client will automatically try to recover from all errors.
+			// Timeout is not considered an error because it is raised by
+			// ReadMessage in absence of messages.
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+
+	c.Close()
 }
---
+++ gokafka/test/kafka_test.go
@@ -0,0 +1,8 @@
+package test
+
+import "testing"
+
+func TestConnKafka(t *testing.T) {
+   
+}
+
