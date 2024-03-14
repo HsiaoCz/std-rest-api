c49b25d913f9f25b41ff47e0f9125b5840097b24
HsiaoCz <984274788@qq.com>
2024-03-15 00:04:32 +0800

    gpf

--- gpf/cmd/main.go
+++ gpf/cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
-	
+import (
+	"flag"
+
+	"github.com/HsiaoCz/std-rest-api/gpf/gg"
+)
+
+func main() {
+	listenAddr := flag.String("listenAddr", "127.0.0.1:9001", "listen address of service")
+	flag.Parse()
+
+	svc := gg.LoggingService{PriceServer: gg.PriceServer{}}
+	server := gg.NewJSONAPIServer(*listenAddr, svc)
+	server.Run()
 }
