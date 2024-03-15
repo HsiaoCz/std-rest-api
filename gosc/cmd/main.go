cb7ec8372498151d6432ac4b29bce50ac3d83ace
HsiaoCz <984274788@qq.com>
2024-03-15 19:35:04 +0800

    template

--- gosc/cmd/main.go
+++ gosc/cmd/main.go
@@ -1,25 +1,94 @@
 package main
 
 import (
+	"encoding/json"
+	"html/template"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/user", makeHTTPHandler(handleUser))
-	http.ListenAndServe(":3001", nil)
+type Context struct {
+	w   http.ResponseWriter
+	req *http.Request
+	t   *Template
 }
 
-type apifunc func(w http.ResponseWriter, r *http.Request) error
+func (c *Context) WriteJSON(code int, value any) error {
+	c.w.Header().Set("Content-Type", "application/json")
+	c.w.WriteHeader(code)
+	return json.NewEncoder(c.w).Encode(value)
+}
+func (c *Context) render(name string, data any) error {
+	return c.t.template.ExecuteTemplate(c.w, name, data)
+}
 
-func makeHTTPHandler(fn apifunc) http.HandlerFunc {
+type apifunc func(c *Context) error
+
+func makeHTTPHandler(fn apifunc, t *Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if err := fn(w, r); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(err.Error()))
+		ctx := &Context{
+			t:   t,
+			req: r,
+			w:   w,
+		}
+		if err := fn(ctx); err != nil {
+			ctx.WriteJSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
 		}
 	}
 }
 
-func handleUser(w http.ResponseWriter, r *http.Request) error {
-	return nil
+var t *Template
+
+type User struct {
+	Username string `json:"username"`
+	IsAdmin  bool   `json:"is_admin"`
+	Age      int    `json:"age"`
+}
+
+type Template struct {
+	template *template.Template
+}
+
+func main() {
+	t = &Template{
+		template: template.Must(template.ParseGlob("www/*.html")),
+	}
+	http.HandleFunc("/user", makeHTTPHandler(handleUser, t))
+	http.HandleFunc("/", makeHTTPHandler(handleHome, t))
+	http.HandleFunc("/cat", makeHTTPHandler(showCatFacts, t))
+	http.ListenAndServe(":3001", nil)
+}
+
+func handleUser(c *Context) error {
+	return c.WriteJSON(http.StatusOK, map[string]any{
+		"message": "hello some user",
+	})
+}
+
+func handleHome(c *Context) error {
+	user := User{
+		Username: "hason",
+		IsAdmin:  true,
+		Age:      31,
+	}
+	return c.render("index.html", user)
+}
+
+type CatFact struct {
+	Fact string `json:"fact"`
+}
+
+func showCatFacts(c *Context) error {
+	resp, err := http.Get("https://catfact.ninja/fact")
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	fact := &CatFact{}
+
+	if err := json.NewDecoder(resp.Body).Decode(fact); err != nil {
+		return err
+	}
+	return c.render("index.html", fact)
 }
